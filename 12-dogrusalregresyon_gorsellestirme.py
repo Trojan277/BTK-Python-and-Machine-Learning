17d5e99b20c6d9fe17d3ab4924dd6cbe619814a3
Trojan277 <70176627+Trojan277@users.noreply.github.com>
2021-02-11 11:20:07 +0300

    Add files via upload

--- 12-dogrusalregresyon_gorsellestirme.py
+++ 12-dogrusalregresyon_gorsellestirme.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 15 04:18:20 2018
-
-@author: sadievrenseker
-"""
-
 #1. kutuphaneler
 import numpy as np
 import matplotlib.pyplot as plt
